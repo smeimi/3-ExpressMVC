eb72f01937aa26e9d0c5129f8aa9227b7c90ff90
smeimi <42933767+smeimi@users.noreply.github.com>
2018-09-24 14:10:02 +0300

    ws3

--- app_server/controllers/ctrlFootball.js
+++ app_server/controllers/ctrlFootball.js
@@ -3,14 +3,14 @@ const winnerlist = function(req, res){
     res.render('football',{
         winners:
         [
-            {year:'1990', team:'Germany'},
-            {year:'1994', team:'Brazil'},
-            {year:'1998', team:'France'},
-            {year:'2002', team:'Brazil'},
-            {year:'2006', team:'Italy'},
-            {year:'2010', team:'Spain'},
-            {year:'2014', team:'Germany'},
-            {year:'2018', team:'Croacia'}
+            {year:'1990', movie:'Misery'},
+            {year:'1994', movie:'Lion King'},
+            {year:'1998', movie:'Deep Impact'},
+            {year:'2002', movie:'8 Mile'},
+            {year:'2006', movie:'Silent Hill'},
+            {year:'2010', movie:'Inception'},
+            {year:'2014', movie:'Interstellar'},
+            {year:'2018', movie:'Venom'}
         ]});
 };
 module.exports = {
--- app_server/controllers/ctrlGoldenBall.js
+++ app_server/controllers/ctrlGoldenBall.js
@@ -3,14 +3,14 @@ const winnerlist = function(req, res){
     res.render('goldenball',{
         winners:
         [
-            {year:'1990', player:'Salvatore Schillaci', team: 'Italy'},
-            {year:'1994', player:'Romário', team: 'Brazil'},
-            {year:'1998', player:'Ronaldo', team: 'Brazil'},
-            {year:'2002', player:'Oliver Khan', team: 'Germany'},
-            {year:'2006', player:'Zinedine Zidane', team: 'France'},
-            {year:'2010', player:'Diego Forlán', team: 'Uruguay'},
-            {year:'2014', player:'Lionel Messi', team: 'Argentina'},
-            {year:'2018', player:'Luka Modric', team: 'Croacia'}
+            {year:'1990', director:'Rob Reiner', movie: 'Misery'},
+            {year:'1994', director:'Ron Minkoff', movie: 'Lion King'},
+            {year:'1998', director:'Mimi Leder', movie: 'Deep Impact'},
+            {year:'2002', director:'Curtis Hanson', movie: '8 Mile'},
+            {year:'2006', director:'Christophe Gans', movie: 'Silent Hill'},
+            {year:'2010', director:'Christopher Nolan', movie: 'Inception'},
+            {year:'2014', director:'Christopher Nolan', movie: 'Interstellar'},
+            {year:'2018', director:'Ruben Fleischer', movie: 'Venom'}
         ]});
 };
 
