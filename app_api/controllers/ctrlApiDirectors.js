222f81cdc0274433b3314cec4bf144e660f3cd6c
smeimi <42933767+smeimi@users.noreply.github.com>
2018-11-01 14:37:41 +0200

    newwest newwerrr

--- app_api/controllers/ctrlApiDirectors.js
+++ app_api/controllers/ctrlApiDirectors.js
@@ -2,12 +2,30 @@ const mongoose = require('mongoose');
 const directorsModel = mongoose.model('directorsWinner');
 
 const winnerList = function (req, res) {
+
+    directorsModel.find({},function(err, winners){
+        if (err){
+            res.status(404).json(err);
+        }
+        else{
+            res.status(200).json(winners);
+        }
+    });
     res
         .status(200)
-        .json({"year" : "movie"});
+        .json({"year" : "director"});
 };
 
 const addWinner = function (req, res) {
+    directorsModel.create(req.body, function(err, newWinner){
+            if (err){
+                res.status(400).json(err);
+            }
+            else{
+                res.status(201).json(newWinner);
+            }
+        }
+    );
     res.status(201).json({"Add winner" : "Work in progress"});
 };
 module.exports = {
--- app_api/controllers/ctrlApiMovies.js
+++ app_api/controllers/ctrlApiMovies.js
@@ -2,12 +2,30 @@ const mongoose = require('mongoose');
 const moviesModel = mongoose.model('moviesWinner');
 
 const winnerList = function (req, res) {
+
+    moviesModel.find({},function(err, winners){
+        if (err){
+            res.status(404).json(err);
+        }
+        else{
+            res.status(200).json(winners);
+        }
+    });
     res
         .status(200)
         .json({"year" : "movie"});
 };
 
 const addWinner = function (req, res) {
+    moviesModel.create(req.body, function(err, newWinner){
+        if (err){
+            res.status(400).json(err);
+        }
+        else{
+            res.status(201).json(newWinner);
+        }
+        }
+    );
     res.status(201).json({"Add winner" : "Work in progress"});
 };
 module.exports = {
