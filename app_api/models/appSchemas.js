e0d4b2ba8ecc7f0c454bbda8280e5092de105f7a
smeimi <42933767+smeimi@users.noreply.github.com>
2018-11-03 15:39:31 +0200

    vittu

--- app_api/models/appSchemas.js
+++ app_api/models/appSchemas.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose');
 const movieSchema = new mongoose.Schema({year:String, movie:String});
 const directorsSchema = new mongoose.Schema({year:String, director:String, movie:String});
 
-mongoose.model('movies', movieSchema, 'movies');
-mongoose.model('directors', directorsSchema, 'directors');
+mongoose.model('moviesModel', movieSchema, 'movies');
+mongoose.model('directorsModel', directorsSchema, 'directors');
