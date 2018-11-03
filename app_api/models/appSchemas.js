b3a40d54b1409c2110d402f6ac746f1e159786da
smeimi <42933767+smeimi@users.noreply.github.com>
2018-11-03 15:26:53 +0200

    fixed schema

--- app_api/models/appSchemas.js
+++ app_api/models/appSchemas.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose');
 const movieSchema = new mongoose.Schema({year:String, movie:String});
 const directorsSchema = new mongoose.Schema({year:String, director:String, movie:String});
 
-mongoose.model('moviesWinner', movieSchema, 'moviesWinners');
-mongoose.model('directorsWinner', directorsSchema, 'directorsWinners');
+mongoose.model('movies', movieSchema, 'movies');
+mongoose.model('directors', directorsSchema, 'directors');
